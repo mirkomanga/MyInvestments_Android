8b9f3f7f66008cf7374b449b881decf577d029b4
dottorgonzo <mirko.manganiello@ibm.com>
2022-01-04 16:46:21 +0100

    Debugging

--- app/src/main/java/com/lalorosas/myinvestments/Investment.kt
+++ app/src/main/java/com/lalorosas/myinvestments/Investment.kt
@@ -66,7 +66,7 @@ class InvestmentDBOpenHelper(context: Context,
         val investments = ArrayList<Investment>()
 
         while(cursor.moveToNext()) {
-		    val check = cursor.getColumnIndex(COLUMN_NAME) != null
+		    val check = cursor.getColumnIndex(COLUMN_NAME) >= 0
             if (check) {
 			    val name = cursor.getString(cursor.getColumnIndex(COLUMN_NAME))
                 val amount = cursor.getFloat(cursor.getColumnIndex(COLUMN_AMOUNT))
